cbeda7fc6db7d572290539d454da1becacc4da82
Fridah <fridahnthambi69@gmail.com>
2025-07-12 03:06:36 +0300

    Fix: Add basic_operations.py correctly without submodule

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#define the numbers
+number1 = 10
+number2 = 5
+
+#perform operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#print the results
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
+
+
