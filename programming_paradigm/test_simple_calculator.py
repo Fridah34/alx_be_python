f3defae2b3113c003c4483de12660f4ecae41a63
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-08-03 16:53:17 +0300

    Fix: Rename test_multiply to test_multiplication for checker

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -15,7 +15,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 2), 3)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(0, 100), 0)
 
